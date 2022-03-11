88ddbe3010fa36fd8e862837877d86c813293352
AlexandrAQA <panaev.as@gmail.com>
2022-03-11 23:34:23 +0400

    Initial commit

---
+++ src/com/tbilisi/Main.java
@@ -0,0 +1,34 @@
+package com.tbilisi;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Party party();
+        System.out.println("Hello Java and World!)");
+        System.out.println("shgdshgdshg      d");
+        System.out.print("A");
+        System.out.println(" a");
+        System.out.print("B");
+        System.out.println(" b");
+        System.out.println();
+        System.out.println("ABCDEFG");
+        System.out.println();  //Add empty string
+        System.out.println("HIJKLNOP");
+        System.out.println("                 РУБАИ              ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                              Омар Хайям");
+
+
+        // 'Ctrl +Shift + /' => comment selected code
+        /*Ctrl+Shift+Enter => complete current code (Statement)
+        *Ctrl+B => Go To Declaration, Navigate to the initial declaration of the instantiated class, called method, or field.
+        */
+
+    }
+}
---
+++ src/com/tbilisi/Party.java
@@ -0,0 +1,14 @@
+package com.tbilisi;
+
+import java.awt.*;
+
+public class Party {
+    public void buildInvite(){
+        Frame frame = new Frame();
+        Label label = new Label("Party in Tbilisi");
+        Button your_bet = new Button("Your bet");
+        Button cancel = new Button("cancel");
+        Panel panel = new Panel();
+        panel.add(label);
+    }
+}
