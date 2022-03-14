e0f8dc5415ce087646b922116cc2bc1478b8dc63
AlexandrAQA <panaev.as@gmail.com>
2022-03-14 14:57:33 +0400

    It was my 5th commit: &&, ||, true, false

--- src/com/tbilisi/Main.java
+++ src/com/tbilisi/Main.java
@@ -3,19 +3,40 @@ package com.tbilisi;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Lesson 3");
-        int a = 11, b = 3, c = 1_000;
-        c = a / b; // int cut all numbers after comma
-        System.out.println(c); // 11 / 3 = 3
+	    System.out.println("Lessons 3 & 4");
+        boolean a = true;
+        boolean b = true;
+        boolean c = true;
+        boolean res = a && b && c; //AND = true if everyone is true!
+        boolean res2 = a || b || c;  //OR = true if one is true
+        System.out.println("res = " + res +  " res2 = " + res2);
 
-        double a2 = 11, b2 = 3, c2;
-        c2 = a2 / b2; // 3.66666666665
-        System.out.println(c2);
+        boolean tr = true;
+        boolean fl = false;
+        tr = !fl;
+        fl = !tr;
+        System.out.println("tr = " + tr +  " fl = " + fl);
+        int i = 10;
+        int i2 = 100;
+        int i3 = 95;
+        boolean jjj = i++ + 100 >= i2 && --i2 > i && i3 != i2;
+        System.out.println(jjj);
 
-        double x = 5.5;
-        double y = 3.8;
-        double z = x % y;
-        System.out.println(z);
+        boolean b08 = true;
+        boolean b09 = true;
+        boolean b10 = true;
+
+        boolean b20 = false;
+        boolean b30 = false;
+        boolean b40 = false;
+        System.out.println(b10^b20^b30^b40); // ^ = true when only 1= true, other = false
+        System.out.println(b08^b09); // ^ = false
+
+        int k = 10;
+        int l = 9;
+        int result;
+        result = 17 - 3 * (l-- + k) / 2;
+        System.out.println(result);
 
 
 
