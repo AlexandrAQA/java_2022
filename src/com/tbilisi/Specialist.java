f2443ab18b0fa21854c384b5b56fa718e369f53c
AlexandrAQA <panaev.as@gmail.com>
2022-03-23 01:18:41 +0400

    It was my 16th commit: "Classes & Objects": Overloaded constructor and use THIS

--- src/com/tbilisi/Specialist.java
+++ src/com/tbilisi/Specialist.java
@@ -9,25 +9,23 @@ public class Specialist {
     String id_security;
 
     Specialist(int id_spec, String sur_name, int age_sp, boolean isHeHavePet) {
+        this(sur_name, age_sp);
+        //we can use this only on the 1st position !!!
         id = id_spec;
-        surname = sur_name;
-        age = age_sp;
         hasPet = isHeHavePet;
     }
     Specialist(int id_spec2, String sur_name2, int age_sp2) {
+        this(sur_name2, age_sp2);
         id = id_spec2;
-        surname = sur_name2;
-        age = age_sp2;
 
     }
     Specialist(String sur_name3, int age_sp3) { // we use this 3RD constructor into 5th
         surname = sur_name3;
         age = age_sp3;
     }
-    protected Specialist(String id_s, String surname_s, int age_s, double salary_s, boolean isHavePet_s){
-        id_security = id_s;
-        surname = surname_s;
-        age = age_s;
+    protected Specialist(String id_sec_str, String surname_s, int age_s, double salary_s, boolean isHavePet_s){
+       this(surname_s, age_s);
+        id_security = id_sec_str;
         salary = salary_s;
         hasPet = isHavePet_s;
     }
@@ -56,10 +54,10 @@ class SpecialistTest {
         System.out.println("id: "+ specialist3.id + specialist3.surname + " age: " + specialist3.age);
 
         Specialist specialist_security = new Specialist( "007", " Bond", 40, 100_000.99, false);
-        System.out.println("id: " + specialist_security.id + " surname: "+ specialist_security.surname);
+        System.out.println("id: " + specialist_security.id_security + " surname: "+ specialist_security.surname);
 
         int r = specialist1.sum(1,2,3)/3;
-        System.out.println(r);
+        System.out.println("Average of 3 numbers is: " + r);
         }
 
         Specialist specialist5 = new Specialist(005, "Bornson", 22, 5_100.78);
