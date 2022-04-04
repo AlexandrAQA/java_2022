50a59002cf60bd59e54ff904721771e2f58f8fd8
AlexandrAQA <panaev.as@gmail.com>
2022-04-04 21:43:25 +0400

    It was my 24th commit

---
+++ src/lesson10/IfElseClass.java
@@ -0,0 +1,29 @@
+package lesson10;
+
+import java.util.Scanner;
+
+public class IfElseClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // 3 Integers if some of them are equal then print them
+        if (a == b && b != c) {
+            System.out.println(a + " " + b);
+        }
+        if (a == c && a != b) {
+            System.out.println(a + " " + c);
+        }
+        if (b == c && a != b) {
+            System.out.println(b + " " + c);
+        }
+        if (a == b && b == c) {
+            System.out.println(a + " " + b + " " + c);
+        }
+
+    }
+
+}
+
+
