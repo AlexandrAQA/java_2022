7690cc9dc2855442c1096c8160ac9bd532cd5a91
AlexandrAQA <panaev.as@gmail.com>
2022-04-15 16:12:31 +0400

    It was my 29th commit, Cycle while which can count sum of all entered numbers until user types ENTER

---
+++ src/lesson12/WhileCycle.java
@@ -0,0 +1,24 @@
+package lesson12;
+
+import java.util.Scanner;
+
+public class WhileCycle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        boolean isExit = false;
+        while (!isExit) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                sum = sum + number;
+                System.out.println("Current sum of the numbers is " + sum);
+            } else if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.equals("ENTER")) {
+                    isExit = true;
+                }
+            }
+        }
+        System.out.println("Total sum of the numbers is " + sum);
+    }}
